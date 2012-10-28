8f32f15bf96e3956437d8434102306517f0b5d7a
saasbook <saasbook@saasbook.(none)>
2012-10-27 17:14:37 -0700

    part 1

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,10 +22,10 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
+  Given I check the following ratings: G, PG, R
+  Given I uncheck the following ratings: PG-13
+#lick_button "submit"
+  # enter step(s) to  ensure that PG and R movies are visible
   # enter step(s) to ensure that other movies are not visible
 
 Scenario: no ratings selected
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,11 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
+    Movie.create!(movie)
+# each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+#flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -22,7 +23,12 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  for rating in rating_list.split(', ') do
+    check_id="ratings_"+rating
+    if uncheck
+      step %Q{I uncheck "#{check_id}"}
+    else
+      step %Q{I check "#{check_id}"}
+    end
+  end
 end
